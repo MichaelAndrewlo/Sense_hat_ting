59b2e2026161b06f6bcc7f63a07aff072a5d40ec
MichaelAndrewlo <29CaseM@kgs.org.uk>
2025-05-14 13:57:58 +0100

    WIP.py

--- da_game.py
+++ da_game.py
@@ -12,6 +12,7 @@ g = 255, 0, 255 # pink/ magenta
 h = 255, 153, 0 # orange 
 i = 255, 255, 255 # white 
 enemy_positions = [] 
+player_positions = [[0, 0, 0, 2],[7, 7, 7, 5]]
 x_pos = 0 
 y_pos = 0 
 pl_x = 0 
@@ -35,20 +36,33 @@ traps = []
   
   
 def ai(t): 
-  global enemy_positions 
+  global enemy_positions
   global x_pos
-  global y_pos 
-  global pl_x 
-  global pl_y 
-  global dead 
-  get_xy(t, enemy_positions) 
+  global y_pos
+  pl_x = player_positions[0][0]
+  pl_y = player_positions[0][1]
+  get_xy(t, enemy_positions)
   org_x = x_pos 
   org_y = y_pos 
-  if x_pos < pl_x: 
+  if players == 1:
+    u_plx = pl_x
+    u_ply = pl_y
+  if players >= 2:
+    dist = get_dist(x_pos, y_pos, pl_x, pl_y)
+    pl2_x = player_positions[1][0]
+    pl2_y = player_positions[1][1]
+    dist2 = get_dist(x_pos, y_pos, pl2_x, pl2_y)
+    if dist < dist2:
+      u_plx = pl_x
+      u_ply = pl_y
+    else:
+      u_plx = pl2_x
+      u_ply = pl2_y
+  if x_pos < u_plx: 
     x_pos += 1 
-  elif x_pos > pl_x: 
+  elif x_pos > u_plx: 
     x_pos -= 1 
-  elif y_pos < pl_y: 
+  elif y_pos < u_ply: 
     y_pos += 1 
   else: 
     y_pos -= 1 
@@ -58,7 +72,7 @@ def ai(t):
     if coords == i: 
       coords = str(org_x) + str(org_y) 
   enemy_positions.insert(t, coords)
-  sense.clear() 
+  sense.clear()
   
 def check_pos(x1): 
   while x1 >= 8: 
@@ -67,130 +81,72 @@ def check_pos(x1):
     x1 += 1 
   return x1 
   
+def get_dist(x, y, x2, y2):
+  x_dist = x - x2
+  y_dist = y - y2
+  x_square = x_dist ** 2
+  y_square = y_dist ** 2
+  square = x_square + y_square
+  dist = square ** (1 / 2)
+  return dist
+  
 def get_xy(z, lists): 
-  global enemy_positions 
   global x_pos 
   global y_pos 
   position = lists[z] 
   x_pos = int(position[0]) 
   y_pos = int(position[1]) 
   
-def move(d, player): 
-  global pl_x
-  global pl_y
-  global pl2_x
-  global pl2_y
-  global score 
-  global enemy_positions 
-  global x_pos 
-  global y_pos 
-  global z 
-  global w 
-  if player == 1:
-    if d == "up": 
-      pl_y -= 1 
-    elif d == "down": 
-      pl_y += 1 
-    elif d == "left": 
-      pl_x -= 1 
-    elif d == "right": 
-      pl_x += 1 
-    pl_x = check_pos(pl_x) 
-    pl_y = check_pos(pl_y) 
-  if player == 2:
-    if d == "up": 
-      pl2_y -= 1 
-    elif d == "down": 
-      pl2_y += 1 
-    elif d == "left": 
-      pl2_x -= 1 
-    elif d == "right": 
-      pl2_x += 1 
-    pl2_x = check_pos(pl2_x) 
-    pl2_y = check_pos(pl2_y) 
-  sense.clear() 
+def move(d, player):
+  global player_positions
+  x_pos = player_positions[player][0]
+  y_pos = player_positions[player][1]
+  if d == "up":
+      y_pos-= 1
+  elif d == "down":
+      y_pos += 1
+  elif d == "left":
+      x_pos -= 1
+  elif d == "right":
+      x_pos += 1
+  x_pos = check_pos(x_pos)
+  y_pos = check_pos(y_pos)
+  player_positions[player][0] = x_pos
+  player_positions[player][1] = y_pos
+  sense.clear()
   
-def spawn_enemies(z): 
-  global pl_x 
-  global pl_y 
-  global enemy_positions 
-  for i in range(z): 
-    x = randint(0,7) 
-    y = randint(0,7) 
-    for i in range(len(enemy_positions)): 
-      get_xy(i, enemy_positions) 
-      while (x_pos == x and y_pos == y) or (x_pos == pl_x and y_pos == pl_y): 
+def spawn_enemies(z):
+  global enemy_positions
+  for i in range(z):
+    x = randint(0,7)
+    y = randint(0,7)
+    for i in range(len(enemy_positions)):
+      get_xy(i, enemy_positions)
+      while (x_pos == x and y_pos == y) or (x_pos == player_positions[0][0] and y_pos == player_positions[0][1]) or (x_pos == player_positions[1][0] and y_pos == player_positions[1][1]): 
         x = randint(0,7) 
         y = randint(0,7) 
     coords = str(x) + str(y) 
     enemy_positions.append(coords) 
-  
-def weapon(x, y, t): 
-  global enemy_positions 
-  global w 
-  global z   
-  global score 
-  global col 
-  if players == 1:
-    for i in range(len(enemy_positions)): 
-      get_xy(i, enemy_positions) 
-      if z == x_pos and w == y_pos: 
-        if t == "middle": 
-          enemy_positions.pop(i) 
-          score += 1 
-        break
-  elif players == 2:
-    if z == pl2_x and w == pl2_y:
-        if t == "middle": 
-          winner = "player 1"
-  if t == "left":
-    z = x - 2
-  elif t == "right": 
-    z = x + 2
-  else:
-    z = x
-  if t == "up":
-    w = y - 2
-  elif t == "down":
-    w = y + 2   
-  else:
-    w = y
-  w = check_pos(w)
-  z = check_pos(z)
-  
-def weapon2(x, y, t): 
-  global enemy_positions 
-  global j
-  global k  
-  global score 
-  global col 
-  global players
-  if players == 1:
-    for i in range(len(enemy_positions)): 
-      get_xy(i, enemy_positions) 
-      if z == x_pos and w == y_pos: 
-        if t == "middle": 
-          enemy_positions.pop(i) 
-          score += 1 
-        break
-  if players == 2:
-    if j == pl_x and k == pl_y:
-        if t == "middle": 
-          winner = "player 2"
-  if t == "left":
-    j = x - 2
-  elif t == "right": 
-    j = x + 2
-  else:
-    j = x
-  if t == "up":
-    k = y - 2
-  elif t == "down":
-    k = y + 2   
-  else:
-    k = y
-  j = check_pos(j)
-  k = check_pos(k)
+
+def weapon():
+    global player_positions
+    x_pos = player_positions[player][0]
+    y_pos = player_positions[player][1]
+    wx_pos = player_positions[player][2]
+    wy_pos = player_positions[player][3]
+    if d == "up":
+        wy_pos = y_pos - 2
+    elif d == "down":
+        wy_pos = y_pos + 2
+    elif d == "left":
+        wx_pos = x_pos - 2
+    elif d == "right":
+        wx_pos = x_pos + 2
+    wx_pos = check_pos(wx_pos)
+    wy_pos = check_pos(wy_pos)
+    player_positions[player][2] = wx_pos
+    player_positions[player][3] = wy_pos
+    print(player_positions)
   
 """
 def place_trap():
@@ -218,12 +174,12 @@ if players == 1:
   while True:
     for i in range(len(enemy_positions)):
       get_xy(i, enemy_positions)
-      if x_pos == pl_x and y_pos == pl_y:
+      if x_pos == player_positions[0][0] and y_pos == player_positions[0][1]:
         dead = True
     for event in sense.stick.get_events():
       if event.action == "pressed":
-        move(event.direction, 1)
-        weapon(pl_x, pl_y, event.direction)
+        move(event.direction, 0)
+        weapon()
     if len(enemy_positions) == 0:
       difficulty += 1
       spawn_enemies(difficulty)
@@ -244,7 +200,7 @@ if players == 1:
         break
       else:
         col = a
-    sense.set_pixel(pl_x, pl_y, b)
+    sense.set_pixel(player_positions[0][0], player_positions[0][1], b)
     sense.set_pixel(z, w, col)
     sleep(0.1)
 
