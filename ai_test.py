6af997f0a1efaed8b963e749a868c13767ed2666
MichaelAndrewlo <29CaseM@kgs.org.uk>
2025-05-15 12:28:22 +0100

    Update ai_test.py

--- ai_test.py
+++ ai_test.py
@@ -1,3 +1,6 @@
+from time import sleep
+player1 = [0, 0]
+player2 = [7, 7]
 def get_dist(x, y, x2, y2): 
   x_dist = x - x2 
   y_dist = y - y2 
@@ -12,4 +15,26 @@ class Enemy:
     self.position = [x, y]
 
   def ai(self):
-    
+    dist = get_dist(self.position[0], self.position[1], player1[0], player1[1])
+    dist2 = get_dist(self.position[0], self.position[1], player2[0], player2[1])
+    pl_x = player1[0]
+    pl_y = player1[1]
+    pl2_x = player2[0]
+    pl2_y = player2[1]
+    if dist < dist2: 
+      u_plx = pl_x 
+      u_ply = pl_y 
+    else:
+      u_plx = pl2_x 
+      u_ply = pl2_y 
+    if self.position[0], < u_plx:  
+      self.position[0], += 1  
+    elif self.position[0], > u_plx:  
+      self.position[0], -= 1  
+    elif self.position[1], < u_ply:  
+      self.position[1] += 1  
+    else:  
+      self.position[1] -= 1  
+
+while True:
+  
